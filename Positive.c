c5a5dd03a1f680d8414a8011c9a44b3fc03df628
preethipoomathi <preethigana@gmail.com>
2016-09-01 20:38:55 +0530

    Create Positive.c

---
+++ Positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+{
+printf("zero");
+}
+elseif(n<0)
+{
+printf("negative");
+}
+else
+{
+printf("positive");
+}
+}
