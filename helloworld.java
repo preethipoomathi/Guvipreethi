b074f98c084f29697381286fa6c72ed601817bef
preethipoomathi <preethigana@gmail.com>
2016-09-13 12:31:12 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Helwithout {
+
+	public static void main(String[] args) {
+		
+		for(System.out.println("HELLOWORLD");;)
+		{
+			//without display function
+		}
+	}
+
+}
