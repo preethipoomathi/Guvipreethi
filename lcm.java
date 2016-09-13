84aece0d72ec5670ce8974d716b4282fbbcd9a44
preethipoomathi <preethigana@gmail.com>
2016-09-13 10:31:20 +0530

    Create lcm.java

---
+++ lcm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class lcm {
+
+	public static void main(String args[])
+	{
+		int lcm=0;
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		for(int i=a;i<=a*b;i++)
+		{
+		if(i%a==0 && i%b==0)
+		{
+			lcm=i;
+			break;
+		}
+		}
+		System.out.println(lcm);
+	}
+}
