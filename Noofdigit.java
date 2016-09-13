0d050299f0031998554ea3644e1097f412cd0791
preethipoomathi <preethigana@gmail.com>
2016-09-13 11:56:38 +0530

    Create Noofdigit.java

---
+++ Noofdigit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NOofdigit {
+
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int c=sc.nextInt();
+String s=String.valueOf(c);
+int n=s.length();
+System.out.println(n);
+}
+
+
+}
