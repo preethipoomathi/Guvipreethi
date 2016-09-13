57c49606ba23d41156ef618876689c017addcc7d
preethipoomathi <preethigana@gmail.com>
2016-09-13 12:33:55 +0530

    Create StringCompare.java

---
+++ StringCompare.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+public class StringCompareWithoutCompare {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+	String s1=sc.nextLine();
+	String s2=sc.nextLine();
+	if(s1.equals(s2))
+	{
+		System.out.println("equal");
+	}
+	else
+	{
+		System.out.println("not equal");
+	}
+}
+}
